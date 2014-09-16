a74df64c43e15305fcf8ef64e1921f75028fcc1d
diego mauricio herrera alzate <diegomao627@gmail.com>
2014-09-16 09:39:00 -0500

    heroku configuration

--- weeat/settings.py
+++ weeat/settings.py
@@ -60,9 +60,31 @@ ROOT_URLCONF = 'weeat.urls'
 WSGI_APPLICATION = 'weeat.wsgi.application'
 
 
+
+# =========== HEROKU CONFIGURATION =====================
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dacg9e30411ue',                      # Or path to database file if using sqlite3.
+        'USER': 'bdkhufgevkawys',                      # Not used with sqlite3.
+        'PASSWORD': 'Y9x6yM9odu1oz4-LBLBdCxhMQk',                  # Not used with sqlite3.
+        'HOST': 'ec2-54-204-36-244.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = 'staticfiles'
+# =========== END HEROKU CONFIGURATION =====================
+
+
+"""
+# =========== LOCAL CONFIGURATION =====================
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -75,6 +97,10 @@ DATABASES = {
 }
 ALLOWED_HOSTS = []
 STATIC_ROOT = ''
+
+# =========== END LOCAL CONFIGURATION =====================
+"""
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
